e527e5734b8b37829af71e66a3b8191fa047676a
antonio teran <teran@mit.edu>
2018-11-16 00:07:57 -0500

    init rigid body class

---
+++ src/RigidBody.h
@@ -0,0 +1,37 @@
+/**
+ * @file RigidBody.h
+ * @brief encapsulates rigid body parameters and properties
+ * @date November 16, 2018
+ * @author Antonio Teran (teran@mit.edu)
+ */
+
+#ifndef RIGIDBODY_H
+#define RIGIDBODY_H
+
+#define <iostream>
+
+class RigidBody {
+
+private:
+
+  /* define variables */
+
+public:
+
+  /* constructors */
+  RigidBody();
+
+  /* destructors */
+  virtual ~RigidBody();
+
+private:
+
+  /* private methods */
+
+}; /*RigidBody class*/
+
+
+
+#endif /*RIGIDBODY_H*/
+
+
